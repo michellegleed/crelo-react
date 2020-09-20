6031b7551f5405dfcad1e311db01ed1fd5669758
Michelle Gleed <gleed.michelle@gmail.com>
2020-09-20 22:36:24 +0800

    Added pledge form to ProjectPage

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -11,55 +11,67 @@ import { dateObjectFormatter, timeLeftFormatter } from "../utils/dateFormatter.j
 
 function ProjectPage() {
 
-    const dateObj = dateObjectFormatter(oneProject.date_created);
-    const timeLeftObj = timeLeftFormatter(oneProject.due_date);
+  const dateObj = dateObjectFormatter(oneProject.date_created);
+  const timeLeftObj = timeLeftFormatter(oneProject.due_date);
 
-    return (
-        <div>
+  return (
+    <div>
 
-          <h1>{oneProject.title}</h1>
+      <h1>{oneProject.title}</h1>
 
-          <div id="creator-details">
-              <p>Created by</p>
-              <img src="" />
-              <h3>{oneProject.user}</h3>
-          </div>
+      <div id="creator-details">
+        <p>Created by</p>
+        <img src="" />
+        <h3>{oneProject.user}</h3>
+      </div>
 
-          <div className="sticky-sidebar">
-              <div id="time-location">
-                  <h6>Location: {oneProject.venue == "" ? `City of ${oneProject.location}` : `${oneProject.venue}`}</h6>
-
-                  <h6>Time Remaining: {timeLeftObj.days} days, {timeLeftObj.hours} hrs</h6>
-            </div>
-            <div id="targets">
-                <h4>Target: ${oneProject.goal_amount}</h4>
-                <h4>${oneProject.current_amount_pledged} pledged</h4>
-                <ProgressBar
-                  percentage={oneProject.current_percentage_pledged}
-                  current={oneProject.current_amount_pledged}
-                  goal={oneProject.goal_amount}
-                />
-            </div>
-          </div>
-
-        <ProjectDetailCard image={oneProject.image} date={dateObj.date} content={oneProject.description} />
-        
-        {oneProject.updates.map((update, index) => {
-          const date = dateObjectFormatter(update.date_posted);
-          return <ProjectDetailCard image={update.image} date={date.date} content={update.content} key={index} />
-        })}
-
-          <div id="pledges">
-              <h3>Pledges:</h3>
-              <div>
-                {oneProject.pledges.map((pledge, index) => {
-                  return <PledgeCard key={index} pledge={pledge} />;
-                })}
-              </div>
-          </div>
-
+      <div className="sticky-sidebar">
+        <div id="targets">
+          <h4>Target: ${oneProject.goal_amount}</h4>
+          <h4>${oneProject.current_amount_pledged} pledged</h4>
+          <ProgressBar
+            percentage={oneProject.current_percentage_pledged}
+            current={oneProject.current_amount_pledged}
+            goal={oneProject.goal_amount}
+          />
         </div>
-    );
+
+        <div id="time-location">
+          <h6>Location: {oneProject.venue == "" ? `City of ${oneProject.location}` : `${oneProject.venue}`}</h6>
+
+          <h6>Time Remaining: {timeLeftObj.days} days, {timeLeftObj.hours} hrs</h6>
+        </div>
+
+        <div id="pledge-form">
+          <h3>Make A Pledge...</h3>
+          <form>
+            <input placeholder="Enter amount" id="pledge-value"></input>
+            <textarea placeholder="Add a comment" id="pledge-comment"></textarea>
+            <label for="anonymous">Keep this anonymous</label>
+            <input type="checkbox" id="anonymous"></input>
+            <button>Submit</button>
+          </form>
+        </div>
+      </div>
+
+      <ProjectDetailCard image={oneProject.image} date={dateObj.date} content={oneProject.description} />
+
+      {oneProject.updates.map((update, index) => {
+        const date = dateObjectFormatter(update.date_posted);
+        return <ProjectDetailCard image={update.image} date={date.date} content={update.content} key={index} />
+      })}
+
+      <div id="pledges">
+        <h3>Pledges:</h3>
+        <div>
+          {oneProject.pledges.map((pledge, index) => {
+            return <PledgeCard key={index} pledge={pledge} />;
+          })}
+        </div>
+      </div>
+
+    </div>
+  );
 }
 
 export default ProjectPage;
